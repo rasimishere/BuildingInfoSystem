ed7fff42167f8f9029e003485fb779b836e9c201
Rasim <90181726+rasimishere@users.noreply.github.com>
2024-05-28 14:30:52 +0200

    Added Building class to manage residents

--- main.py
+++ main.py
@@ -8,3 +8,15 @@ class Resident:
         return (f"Resident(roomNumber={self.roomNumber}, "
                 f"numberOfResidents={self.numberOfResidents}, "
                 f"rent={self.rent})")
+
+class Building:
+    def __init__(self, address):
+        self.address = address
+        self.residents = []
+
+    def add_resident(self, resident):
+        self.residents.append(resident)
+
+    def __repr__(self):
+        return (f"Building(address='{self.address}', "
+                f"residents={self.residents})")
