46534d48f85b4ef03009b2075b107c9cd3c3969c
Rasim <90181726+rasimishere@users.noreply.github.com>
2024-05-28 14:30:02 +0200

    Created Resident class

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+class Resident:
+    def __init__(self, roomNumber, numberOfResidents, rent):
+        self.roomNumber = roomNumber
+        self.numberOfResidents = numberOfResidents
+        self.rent = rent
 
+    def __repr__(self):
+        return (f"Resident(roomNumber={self.roomNumber}, "
+                f"numberOfResidents={self.numberOfResidents}, "
+                f"rent={self.rent})")
